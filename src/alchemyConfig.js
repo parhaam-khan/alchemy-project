00348a4d2f2e09a8515df57fa5cb1d6e8eaa9357
abc <abc@example.com>
2023-03-07 00:59:20 +0330

    change to ARB chain

--- src/alchemyConfig.js
+++ src/alchemyConfig.js
@@ -2,7 +2,7 @@ const { Alchemy, Network } = require("alchemy-sdk");
 
 const settings = {
     apiKey: "TKC_yfVqek8Uywl_DyIGw7bjFr4Vjo3-",
-    network: Network.ETH_MAINNET,
+    network: Network.ARB_MAINNET,
 };
 
 export const alchemy = new Alchemy(settings);
